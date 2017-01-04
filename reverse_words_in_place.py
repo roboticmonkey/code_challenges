f86ed20065fb96c9b02887973a1731689ca12fb0
roboticmonkey <aiden.ward@gmail.com>
2017-01-04 13:24:25 -0800

    refactoring code

--- reverse_words_in_place.py
+++ reverse_words_in_place.py
@@ -1,8 +1,11 @@
-def reverse_letters(letters):
+
+def reverse_letters(message, start_index, end_index):
+
+    """helper function that reverses letters in a string"""
 
     temp_letter = " "
 
-    for i in xrange(len(letters)/2):
+    for i in xrange(len(letters[start_index: end_index])/2):
         temp_letter = letters[i]
         letters[i] = letters[-(i+1)]
         letters[-(i+1)] = temp_letter
@@ -14,76 +17,29 @@ def reverse_words(message):
 
     """ reverses the words of a message in place """
 
-    # print "before split", message
-
-    # message = message.split(" ")
     message = list(message)
 
-    # print "after split", message
-
-    # temp_letter = " "
-
-    # for i in xrange(len(message)/2):
-    #     temp_letter = message[i]
-    #     message[i] = message[-(i+1)]
-    #     message[-(i+1)] = temp_letter
-
-    message = reverse_letters(message)
-
-    # print "after loop", message
-
-    # message = "".join(message)
-
-    # print " after join", message
+    message = reverse_letters(message, 0, len(message) -1)
 
     word_start = 0
-    word_end = 0
+    
 
     for i in xrange(len(message)):
         # print i, message[i]
 
         if message[i] == " ":
-            word_end = i-1
-            # print "word_end", word_end
             
-            # word_len = i - word_start
-            # # print "word_len", word_len
-            # count = 0
-            # while count < word_len/2:
-            #     # print "count", count
-            #     temp_letter = message[word_start+count]
-            #     # print "temp_letter", temp_letter
-
-            #     message[word_start+count] = message[word_end-count]
-            #     # print "first swap", message[word_start+count]
-            #     message[word_end-count] = temp_letter
-            #     # print "second swap", message[word_end-count]
-            #     count += 1
-            # word_start = i+1
+            message = reverse_letters(message, word_start, i)
+            word_start = i +1
         elif  i == len(message)-1:
-            word_end = i
-            word_len = len(message) - word_start
-            # print "word_len", word_len
-            count = 0
-            while count < word_len/2:
-                # print "count", count
-                temp_letter = message[word_start+count]
-                # print "temp_letter", temp_letter
-
-                message[word_start+count] = message[word_end-count]
-                # print "first swap", message[word_start+count]
-                message[word_end-count] = temp_letter
-                # print "second swap", message[word_end-count]
-                count += 1
-            word_start = i+1
+            
+            message = reverse_letters(message, word_start, i)
+            
         else:
             continue
-    # return message
-
-    
+ 
 
     message = "".join(message)
-    # print message
 
     return message
 
